8132d87c4403d98347dc532698832c74b1617dd7
JavierCastillaGarrido <115453019+JavierCastillaGarrido@users.noreply.github.com>
2024-06-16 23:27:48 +0200

    [ADD]Inicio de css

--- frontestudio/src/App.vue
+++ frontestudio/src/App.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="pagina">
-    <div class="menu">
+    <div id="menu">
       <nav>
-        <router-link to="/" class="objMenu">Home</router-link> |
-        <router-link to="/galeria" class="objMenu">Galeria</router-link> |
-        <router-link to="/tatuadores" class="objMenu">Tatuadores</router-link> |
-        <router-link to="/nosotros" class="objMenu">Sobre Nosotros</router-link> |
-        <router-link to="/login" class="objMenu">Login</router-link>
-      </nav>
+        <div class="objMenu">
+          <router-link to="/">Home</router-link> 
+        </div>
+        <div class="objMenu">
+          <router-link to="/galeria">Galeria</router-link> 
+        </div>
+        <div class="objMenu"> 
+          <router-link to="/tatuadores">Tatuadores</router-link> 
+        </div>
+        <div class="objMenu">
+          <router-link to="/nosotros">Sobre Nosotros</router-link> 
+        </div>
+        <div class="objMenu nolinea">
+          <router-link to="/login">Login</router-link>
+        </div>
+      </nav> 
     </div>
     <router-view />
+    <div id="footer">
+      <div class="copy">
+        ©2023-2024 Drawtatoo - Javier - Jorge | Drawtatoo es una empresa de tatuajes | All rights reserved - Todos los derechos reservados
+      </div>
+      <div class="redes">
+        <div>
+          <a href="#"><img src="assets/icon-facebook.png" alt="face"></a>
+        </div>
+        <div>
+          <a href="#"><img src="assets/icon-instagram.png" alt="insta"></a>
+        </div>
+        <div>
+          <a href="#"><img src="assets/icon-twitter.png" alt="twt"></a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,6 +49,17 @@ export default {
 </script>
 
 <style>
+
+html, body {
+      overflow: auto;
+      scrollbar-width: none;
+      -ms-overflow-style: none;  
+}
+
+html::-webkit-scrollbar, body::-webkit-scrollbar {
+  display: none; 
+}
+
 body {
   margin: 0;
   background-image: url("assets/fondo.jpg");
@@ -31,30 +68,72 @@ body {
   background-position: center center;
 }
 
+
+a{
+  text-decoration: none;
+}
+
 .pagina {
   height: 100vh;
 }
 
-.menu {
+#menu, #footer{
+  display: flex;
+  flex-direction: column;
   background-color: rgba(51, 51, 51, 0.8);
-  padding: 10px 0;
+  align-items: center;
+  height: 6vh;
+}
+
+nav{
+  display: flex;
+  flex-direction: row;
+  width: 60%;
+  margin: auto;
+}
+
+.objMenu{
+  width: 20%;
+  margin: 0;
+  padding: 1% 0;
   text-align: center;
-  height: 75px;
-  font-size: 3em;
+  border-right: 2px solid black;
 }
 
-.menu nav {
-  display: inline-block;
+.nolinea{
+  border: 0;
 }
 
-.objMenu {
+.objMenu a{
+  padding: 2% 25%;
   color: white;
-  text-decoration: none; 
-  margin: 0 15px; 
-  font-size: 18px; 
+  font-size: 20px; 
 }
 
-.objMenu:hover {
-  color: #ff6347;
+.objMenu a:hover {
+  padding: 2% 23%;
+  transition: 0.5s;
+  color: #fd5437;
+  font-size: 21px; 
 }
+
+#footer{
+  clear: both;
+  flex-direction: row;
+}
+
+.copy{
+  width: 80%;
+  color: white;
+  font-size: smaller;
+  text-align: center;
+}
+
+.redes{
+  display: flex;
+  flex-direction: row;
+  margin: auto;
+  justify-content: space-between;
+}
+
 </style>
--- frontestudio/src/views/InicioVista.vue
+++ frontestudio/src/views/InicioVista.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <h1>ESTUDIO TATTOOS</h1>
+  <div id="contenerdorInicio">
+    <div id="imagenLogo">
+      <img src="../assets/Logo.png" alt="logo">
+    </div>
+    <h1>Drawtatoo</h1>
+    <div id="texto"> 
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dignissimos laudantium dolorem, non, in ipsam reprehenderit dolore accusantium tempora eveniet accusamus consequuntur doloremque blanditiis hic rem optio ea. Eaque impedit itaque consequatur, doloribus inventore, aperiam magnam necessitatibus saepe modi tenetur ratione? Repellendus expedita accusantium facere. Nisi non aspernatur ea ab inventore commodi architecto optio, porro animi et consequatur veniam quia voluptates nobis, quidem, tenetur assumenda facere. Similique iure doloremque porro a iusto nostrum ducimus blanditiis molestiae hic sed saepe magnam maiores illo, illum labore amet possimus error, at eius? Unde facere, consectetur quos autem adipisci perspiciatis doloremque quasi fugit voluptas? Corrupti ipsa quod et porro error accusantium libero, culpa deserunt nostrum ipsum recusandae! Perspiciatis vitae exercitationem minus eum nostrum est! Consectetur, dolor dicta. Dolores dolorum quia voluptatum doloremque nobis totam quisquam perspiciatis qui laudantium laborum harum modi ipsum incidunt, tempore ipsa dolore inventore quas accusantium. Temporibus numquam reprehenderit quae repudiandae omnis porro magni maiores veniam. Illo explicabo quibusdam pariatur eveniet, ab delectus adipisci. Cumque placeat optio ratione ea necessitatibus facilis nobis magnam aliquid reprehenderit quas aperiam provident id deserunt quisquam consectetur officia mollitia excepturi corrupti asperiores, fuga possimus quidem. Voluptatibus, dolorum? Perferendis at quis fugiat earum nulla quo adipisci pariatur voluptate soluta ea, quae, sunt optio quam sapiente quasi vero quibusdam amet quisquam. Qui veritatis eaque, beatae, est ipsum tempore fuga cupiditate saepe obcaecati ab recusandae ipsa veniam voluptate, numquam itaque asperiores? Inventore mollitia ipsa facere obcaecati ullam ut quo expedita reiciendis, architecto odit alias ab! Cumque aut nobis excepturi.</p>
+    </div>
   </div>
 </template>
 
@@ -11,11 +17,38 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  color: white;
+
+#contenerdorInicio{
+  display: flex;
+  flex-direction: column;
   text-align: center;
-  margin-top: 10%;
+  color: white;
+  padding: 5% 0.01%;
+}
+
+#imagenLogo img{
+  border-radius: 50%;
+  width: 15%;
+}
+
+h1 {
   font-size: 10em;
+  font-family: 'Times New Roman', Times, serif;
+  margin-top: 0;
+}
+
+#texto{
+  margin: 2.5% auto 2.5%;
+  width: 80%;
+  color: black;
+  background-color: azure;
+  border-radius: 18px;
+  
+}
+
+#texto p{
+  padding: 2% 5%;
+  text-align: justify;
 }
 
 </style>
